af58d24ded67ea7cef258875a025fd5edfc0e84e
Malaclypse <malaccoda@top-email.net>
2024-10-21 12:22:19 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxthq/studio'],
+  modules: [
+    '@nuxthq/studio',
+    '@nuxt/content'
+  ],
   routeRules: {
     '/': { prerender: true }
   },
